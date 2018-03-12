2dc9d2942f89e3906b1a76d3026d8508ef4c74d1
Vitali Zhytniakivskyi <vitalka@mac-admin.local>
2018-03-12 11:20:31 +0300

    Initial Commit

---
+++ Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by Vitali Zhytniakivskyi on 12.03.18.
+//  Copyright © 2018 Vitali Zhytniakivskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
